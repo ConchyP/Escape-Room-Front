6aed811ea5cb37fa6aaa4d3b6ce0682626d9b0e0
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-28 20:42:25 +0200

    Added card template

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div class="container">
+      <div v-for="(escapeRoom, index) in escapeRooms" :key="escapeRoom.id" class="custom-card">
+        <div class="card-left">
+          <img
+            class="card-img-left"
+            :src="escapeRoom.image"
+            alt="Card image"
+          />
+        </div>
+        <div class="card-right">
+          <h4 class="card-title">
+            {{ escapeRoom.nombre }}
+          </h4>
+          <p class="card-difficulty">
+            Dificultad: {{ escapeRoom.dificultad }}
+            <!-- Mostrar llaves según la dificultad -->
+            <span v-if="getDifficultyLevel(escapeRoom.dificultad) === 1">🔑</span>
+            <span v-if="getDifficultyLevel(escapeRoom.dificultad) === 2">🔑🔑</span>
+            <span v-if="getDifficultyLevel(escapeRoom.dificultad) === 3">🔑🔑🔑</span>
+          </p>
+          <div class="btn-container">
+            <button class="btn-play">
+              JUGAR
+            </button>
+          </div>
+          <p class="card-description">
+            {{ escapeRoom.descripcion }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </template>
